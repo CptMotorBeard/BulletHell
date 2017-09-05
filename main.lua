c1cdb801cc61807c17ac4f36edb172ceec302eef
Antonio La Rocca <roccanator@gmail.com>
2017-09-05 14:56:19 -0400

    Draw animated player sprite

--- main.lua
+++ main.lua
@@ -1,5 +1,25 @@
 gameIsPaused = false
 
+PlayerSprite = {
+	love.graphics.newImage("assets/Spaceship1.png"),
+	love.graphics.newImage("assets/Spaceship2.png"),
+	love.graphics.newImage("assets/Spaceship3.png")
+}
+
+Player = {
+	x = 0,
+	y = 0,
+	currentframe = 0.5,
+	Sprite = {
+		PlayerSprite[1],
+		PlayerSprite[2],
+		PlayerSprite[3],
+		PlayerSprite[2]
+	}
+}
+
+SpriteSpeed = 6
+
 -- love.load is called once at the beginning
 function love.load(arg)
 end
@@ -7,10 +27,18 @@ end
 -- dt is delta time, time since function last called. love.update is called every update. math goes here
 function love.update(dt)
 	if gameIsPaused then return end
+	
+	-- update frames
+	if ((Player.currentframe + (SpriteSpeed * dt)) < 4.5) then
+		Player.currentframe = Player.currentframe + (SpriteSpeed * dt)
+	else
+		Player.currentframe = 0.5
+	end
 end
 
 -- love.draw is called every update. graphics go here
 function love.draw(dt)
+	love.graphics.draw(Player.Sprite[math.floor(Player.currentframe + 0.5)], Player.x, Player.y)
 end
 
 -- love.keypressed is called when a key is pressed likewise with keyreleased
