d9338170962e42a647153736c03063f0a6ffc547
Antonio <roccanator@gmail.com>
2017-09-21 15:03:26 -0400

    Player class

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 require 'objects'
 require 'game'
+require 'player'
 
 -- We have our game modes and game states
 game = Game()
@@ -17,50 +18,8 @@ GameMode = 'MainMenu'
 		TODO:
 		Make a player class
 ]]--
-Spaceship = {
-	love.graphics.newImage("assets/Spaceship/Spaceship1.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship2.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship3.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship1Right.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship2Right.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship3Right.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship1Left.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship2Left.png"),
-	love.graphics.newImage("assets/Spaceship/Spaceship3Left.png")
-}
 
-PlayerSpriteNeutral = {
-	Spaceship[1],
-	Spaceship[2],
-	Spaceship[3],
-	Spaceship[2]
-}
-
-PlayerSpriteRight = {
-	Spaceship[4],
-	Spaceship[5],
-	Spaceship[6],
-	Spaceship[5]
-}
-
-PlayerSpriteLeft = {
-	Spaceship[7],
-	Spaceship[8],
-	Spaceship[9],
-	Spaceship[8]
-}
-
-Player = {
-	x = 250,
-	y = 500,
-	currentframe = 0.5,
-	Sprite = PlayerSpriteNeutral,
-	speed = 250,
-	shootpoint = 0,
-	shooting = false,
-	hitbox = Circle(0, 100, 5, 0, 230),
-	Bullets = {}
-}
+Player = Player()
 
 -- Variable to slow down the bullets
 SpriteSpeed = 6
@@ -84,56 +43,9 @@ function love.update(dt)
 	if GameMode == 'MainMenu' then
 	elseif GameMode == 'Play' then
 		GameMode = game:Play(dt)
-
-		Player.Sprite = PlayerSpriteNeutral
-
-		-- Bullets
-		if love.keyboard.isDown('space') then
-			if not Player.shooting then
-				table.insert(Player.Bullets, Circle(Player.shootpoint, Player.y, 3))
-				Player.shooting = true
-			else
-				Player.shooting = false
-			end
-		end
-
-		-- Update player bullets
-		for index, bullet in ipairs(Player.Bullets) do
-			bullet.y = bullet.y - (300 * dt)
-			if bullet.y <= 0 then
-				table.remove(Player.Bullets, index)
-			end
-		end
-
-		-- Movement left / right
-		if love.keyboard.isDown('d') then
-			if not ((Player.x + (Player.speed * dt)) >= (love.graphics.getWidth() - Player.Sprite[1]:getWidth())) then
-				Player.x = Player.x + (Player.speed * dt)
-				Player.Sprite = PlayerSpriteRight
-			end
-		elseif love.keyboard.isDown('a') then
-			if not ((Player.x - (Player.speed * dt)) <= 0) then
-				Player.x = Player.x - (Player.speed * dt)
-				Player.Sprite = PlayerSpriteLeft
-			end
-		end
 		
-		-- Movement up / down
-		if love.keyboard.isDown('w') then
-			if not ((Player.y - (Player.speed * dt)) <= 0) then
-				Player.y = Player.y - (Player.speed * dt)
-			end
-		elseif love.keyboard.isDown('s') then
-			if not ((Player.y + (Player.speed * dt)) >= (love.graphics.getHeight() - Player.Sprite[1]:getHeight())) then
-				Player.y = Player.y + (Player.speed * dt)
-			end
-		end
-
-		-- Set hitbox and bullet entry location to spots on the player sprite
-		Player.hitbox.x = Player.x + (Player.Sprite[1]:getWidth()/2)
-		Player.hitbox.y = Player.y + (Player.Sprite[1]:getHeight()/2)
-
-		Player.shootpoint = Player.x + (Player.Sprite[1]:getWidth()/2)
+		Player:shoot(dt)
+		Player:move(dt)
 
 		-- Check Collisions
 		if game.enemies then
@@ -157,13 +69,6 @@ function love.update(dt)
 					end
 				end
 			end
-		end
-		
-		-- update frames for player sprite
-		if ((Player.currentframe + (SpriteSpeed * dt)) < 4.5) then
-			Player.currentframe = Player.currentframe + (SpriteSpeed * dt)
-		else
-			Player.currentframe = 0.5
 		end
 	end
 end
@@ -198,7 +103,8 @@ function love.draw()
 	-- Draw all the sprites, bullets and hitboxes
 		love.graphics.printf(love.timer.getFPS(), -10, 10, love.graphics.getWidth(), 'right')
 		love.graphics.printf('SCORE :  ' .. __SCORE, 5, 10, love.graphics.getWidth(), 'left')
-		love.graphics.draw(Player.Sprite[math.floor(Player.currentframe + 0.5)], Player.x, Player.y)
+		
+		Player:draw()
 		
 		if hitboxvisible then Player.hitbox:draw() end
 		
---
+++ player.lua
@@ -0,0 +1,113 @@
+Player = {}
+Player.__index = Player
+
+Spaceship = {
+	love.graphics.newImage("assets/Spaceship/Spaceship1.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship2.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship3.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship1Right.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship2Right.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship3Right.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship1Left.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship2Left.png"),
+	love.graphics.newImage("assets/Spaceship/Spaceship3Left.png")
+}
+
+PlayerSpriteNeutral = {
+	Spaceship[1],
+	Spaceship[2],
+	Spaceship[3],
+	Spaceship[2]
+}
+
+PlayerSpriteRight = {
+	Spaceship[4],
+	Spaceship[5],
+	Spaceship[6],
+	Spaceship[5]
+}
+
+PlayerSpriteLeft = {
+	Spaceship[7],
+	Spaceship[8],
+	Spaceship[9],
+	Spaceship[8]
+}
+
+function Player.new(x, y, speed)
+	return setmetatable({
+		x = x or 250,
+		y = y or 500,
+		currentframe = 0.5,
+		Sprite = PlayerSpriteNeutral,
+		speed = speed or 250,
+		shootpoint = 0,
+		shooting = false,
+		hitbox = Circle(0, 100, 5, 0, 230),
+		Bullets = {}},
+	Player)
+end
+
+setmetatable(Player, {__call = function(_, ...) return Player.new(...) end})
+
+function Player:shoot(dt)
+	if love.keyboard.isDown('space') then
+		if not self.shooting then
+			table.insert(self.Bullets, Circle(self.shootpoint, self.y, 3))
+			self.shooting = true
+		else
+			self.shooting = false
+		end
+	end
+	-- Update player bullets
+	for index, bullet in ipairs(self.Bullets) do
+		bullet.y = bullet.y - (300 * dt)
+		if bullet.y <= 0 then
+			table.remove(self.Bullets, index)
+		end
+	end
+end
+
+function Player:move(dt)
+	Player.Sprite = PlayerSpriteNeutral
+	-- Movement left / right
+	if love.keyboard.isDown('d') then
+		if not ((self.x + (self.speed * dt)) >= (love.graphics.getWidth() - self.Sprite[1]:getWidth())) then
+			self.x = self.x + (self.speed * dt)
+			self.Sprite = PlayerSpriteRight
+		end
+	elseif love.keyboard.isDown('a') then
+		if not ((self.x - (self.speed * dt)) <= 0) then
+			self.x = self.x - (self.speed * dt)
+			self.Sprite = PlayerSpriteLeft
+		end
+	end
+	
+	-- Movement up / down
+	if love.keyboard.isDown('w') then
+		if not ((self.y - (self.speed * dt)) <= 0) then
+			self.y = self.y - (self.speed * dt)
+		end
+	elseif love.keyboard.isDown('s') then
+		if not ((self.y + (self.speed * dt)) >= (love.graphics.getHeight() - self.Sprite[1]:getHeight())) then
+			self.y = self.y + (self.speed * dt)
+		end
+	end
+	
+	-- Set hitbox and bullet entry location to spots on the player sprite
+	self.hitbox.x = self.x + (self.Sprite[1]:getWidth()/2)
+	self.hitbox.y = self.y + (self.Sprite[1]:getHeight()/2)
+
+	self.shootpoint = self.x + (self.Sprite[1]:getWidth()/2)
+	
+	-- update frames for player sprite
+	if ((self.currentframe + (SpriteSpeed * dt)) < 4.5) then
+		self.currentframe = self.currentframe + (SpriteSpeed * dt)
+	else
+		self.currentframe = 0.5
+	end
+end
+
+function Player:draw()
+	love.graphics.draw(Player.Sprite[math.floor(Player.currentframe + 0.5)], Player.x, Player.y)
+end
